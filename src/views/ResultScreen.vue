7f5ca5633c775f35f0d96974e8deaa6f1509d9ad
André Ottosen <andre.ottosen@no.experis.com>
2020-09-20 17:02:18 +0200

    Implemented functonality for the final screen view including username, score and new game button.

--- src/views/ResultScreen.vue
+++ src/views/ResultScreen.vue
@@ -3,8 +3,9 @@
     <section id="result-screen-section">
       <img id="logo" alt="logo" src="../assets/logo.jpg">
       <h1>Results</h1>
-      <input type="text" v-model="inputName" id="input-name" placeholder="Name"/>
-      <button id="btn-start" v-on:click="getName">Start</button>
+      <h2>Player: {{user}}</h2>
+      <h3> Score: {{correctAnswers}}/{{numberQuestions}}</h3>   
+      <button type="button" id="btn-newGame" v-on:click="newGame">New game</button> 
     </section>
   </div>
 </template>
@@ -14,10 +15,24 @@ export default {
   name: 'ResultScreen',
   props: {
   },
+   data(){
+    return{
+        correctAnswer: String,
+        numberQuestions: String,
+        user: String
+    }
+  },
+  created(){
+    this.numberQuestions = parseInt(sessionStorage.getItem('numberQuestions'),10);
+    this.correctAnswers = parseInt(sessionStorage.getItem('correctAnswers'),10);
+    this.user = sessionStorage.getItem('user');
+  },
   methods:{
-    getName(){
-      sessionStorage.setItem('user', this.inputName);
-      console.log(sessionStorage.getItem('user'));
+    newGame(){
+      this.$router.push({
+        name: 'StartScreen'
+      }) 
+      sessionStorage.clear();
     }
   }
 }
@@ -42,6 +57,30 @@ export default {
     margin:0.0rem;
     margin-top:1rem;
     margin-left:0rem;
+ }
+ h2{
+    width:100%;
+    font-family: sans-serif;
+    color:#404040;
+    font-weight: 800;
+    letter-spacing: 0.1rem;
+    font-size: 1.4rem;
+    margin:0.0rem;
+    margin-top:0.4rem;
+    margin-left: auto;
+    margin-right:auto; 
+ }
+  h3{
+    width:100%;
+    font-family: sans-serif;
+    color:orange;
+    font-weight: 900;
+    letter-spacing: 0.1rem;
+    font-size: 1.8rem;
+    margin:0.0rem;
+    margin-top:0.0rem;
+    margin-left: auto;
+    margin-right:auto; 
  }
  
 #result-screen-section{
@@ -72,24 +111,24 @@ export default {
     align-content: flex-end;
  }
 
-#btn-start{
+#btn-newGame{
   font-family: Arial, Helvetica, sans-serif;
   font-weight: 600;
   letter-spacing: 0.1rem;
   background-color: orange;
   color:white;
   border: none;
-  margin-top: 0.3rem;
+  margin-top: 1rem;
   padding: 0.2rem;
-  width:100%;
+  width:90%;
   height:2.5rem;
   font-size: 1.1rem;
   margin-left:auto;
   margin-right:auto;
 }
 
-  #btn-start:hover{
-    background-color: gray;
+  #btn-newGame:hover{
+    background-color: darkslateblue;
     cursor: pointer;
   }
 </style>
